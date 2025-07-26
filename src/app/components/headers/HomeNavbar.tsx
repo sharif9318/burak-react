a2ebbbb60489ce88133302efb30dbfdf4799de99
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-07-27 04:03:42 +0900

    homeNavbar menu part refactoring

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -2,35 +2,22 @@ import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 export function HomeNavbar() {
-  const authMember = null;
+  const authMember = true;
 
   return (
     <div className="home-navbar">
-      <Container sx={{ mt: "55px", height: "642px" }}>
-        <Stack
-          sx={{ height: "50px" }}
-          flexDirection={"row"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-        >
+      <Container className="navbar-container">
+        <Stack className="menu">
           <Box>
             <NavLink to="/">
-              <img
-                style={{ width: "125px", height: "30px" }}
-                src="/icons/burak.svg"
-              />
+              <img className="brand-logo" src="/icons/burak.svg" />
             </NavLink>
-          </Box>       
-        <Stack
-          flexDirection={"row"}
-  justifyContent="space-between"
-  minWidth={"700px"} 
-  alignItems="center"
->
-  <Box className={"hover-line"}>
-    <NavLink to="/" activeClassName={"underline"}>
-      Home
-    </NavLink>
+          </Box>
+        <Stack className="links">
+          <Box className={"hover-line"}>
+            <NavLink to="/" activeClassName={"underline"}>
+              Home
+            </NavLink>
   </Box>
   <Box className={"hover-line"}>
     <NavLink to="/products" activeClassName={"underline"}>
@@ -62,13 +49,14 @@ export function HomeNavbar() {
   <Box>
     <Button
       variant="contained"
-      style={{ background: "#3776CC", color: "#f8f8ff" }}
-    >
+    className="login-button">
       Login
     </Button>
   </Box>
 ) : (
-  <img />
+  <img className="user-avatar"
+   src={"/icons/default-user.svg"} 
+   aria-haspopup={"true"} />
 )}
 </Stack>
 </Stack>
