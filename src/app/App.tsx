27b67753a0b68fdc6251f73bd531e7dcdacd9871
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-07-27 05:40:02 +0900

    feat: integration footer and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
-import { HomePage } from "./screens/homePage";
-import { HelpPage } from "./screens/helpPage";
-import { UserPage } from "./screens/userPage";
-import { ProductsPage } from "./screens/productsPage";
-import { OrdersPage } from "./screens/ordersPage";
-import { HomeNavbar } from "./components/headers/HomeNavbar";
-import { OtherNavbar } from "./components/headers/OtherNavbar";
-import { Footer } from "./components/footer";
+import  HomePage from "./screens/homePage";
+import HelpPage from "./screens/helpPage";
+import UserPage from "./screens/userPage";
+import ProductsPage from "./screens/productsPage";
+import OrdersPage from "./screens/ordersPage";
+import HomeNavbar from "./components/headers/HomeNavbar";
+import OtherNavbar from "./components/headers/OtherNavbar";
+import Footer from "./components/footer";
 import "../css/app.css";
 import "../css/navbar.css";
+import "../css/footer.css";
 
 function App() {
 const location = useLocation();
-console.log("Current location:", location.pathname);
+console.log("Current location:" , location.pathname);
 
 
 return (
--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,7 +1,8 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import Basket from "./Basket";
 
-export function OtherNavbar() {
+export default function OtherNavbar() {
     const authMember = null;
     return <div className="other-navbar">
       <Container className="navbar-container">
@@ -41,8 +42,7 @@ export function OtherNavbar() {
     Help
   </NavLink>
 </Box>
-{/* BASKET */}
-
+<Basket />
 {!authMember ? (
   <Box>
     <Button
