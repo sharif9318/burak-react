f6019fb045d06d3066f5e3cb32a767408f2e7ede
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-07-27 10:34:32 +0900

    feat: organize homePage's sectional components

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,5 +1,20 @@
-import { Container } from "@mui/material";
+import React from "react";
+import Statistics from "./Statistics"; // Make sure ./Statistics.tsx exists in the same folder as this file
+import PopularDishes from "./PopularDishes";
+import NewDishes from "./NewDishes";
+import Advertisement from "./Advertisement";
+import ActiveUsers from "./AciveUsers";
+import Events from "./Events";
 
 export default function HomePage() {
-  return <Container>HomePage</Container>;
+  return (
+    <div className={"homepage"}>
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
 }
