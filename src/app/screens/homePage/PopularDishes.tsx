e968d3cc01a9352e0e03dc9b85b54e4bb3000dbc
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-07-30 15:27:59 +0900

    fix: modify newDishes and pupularDishes components

--- src/app/screens/homePage/PopularDishes.tsx
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -18,75 +18,77 @@ const list = [
 
 export default function PopularDishes() {
   return (
-<div className="popular-dishes-frame">
-  <Container>
-    <Stack className="popular-section">
-      <Box className="category-title">Popular Dishes</Box>
-      <Stack className="cards-frame">
-        {list.map((ele, index) => {
-          return (
-            <CssVarsProvider key={index}>
-              <Card className="card">
-                <CardCover>
-                  <img src={ele.imagePath} alt="" />
-                </CardCover>
-                <CardCover className="card-cover" />
-                <CardContent sx={{ justifyContent: "flex-end" }}>
-                  <Stack
-                    flexDirection="row"
-                    justifyContent="space-between"
-                  >
-                    <Typography
-                      level="h2"
-                      fontSize="lg"
-                      textColor="#fff"
-                      mb={1}
-                    >
-                      {ele.productName}
-                    </Typography>
-                    <Typography
-                      sx={{
-                        fontWeight: "md",
-                        color: "neutral.300",
-                        alignItems: "center",
-                        display: "flex",
-                      }}
-                    >
-                      20
-                      <VisibilityIcon
-                        sx={{ fontSize: 25, marginLeft: "5px" }}
-                      />
-                    </Typography>
-                  </Stack>
-                  <Stack>
-                    <CardContent>
-                      <CardOverflow
-                        sx={{
-                          display: "flex",
-                          gap: 1.5,
-                          py: 1.5,
-                          px: "var(--Card-padding)",
-                          borderTop: "1px solid",
-                          height: "60px",
-                        }}
+    <div className="popular-dishes-frame">
+      <Container>
+        <Stack className="popular-section">
+          <Box className="category-title">Popular Dishes</Box>
+          <Stack className="cards-frame">
+            {list.length !== 0 ? (
+              list.map((ele, index) => (
+                <CssVarsProvider key={index}>
+                  <Card className="card">
+                    <CardCover>
+                      <img src={ele.imagePath} alt="" />
+                    </CardCover>
+                    <CardCover className="card-cover" />
+                    <CardContent sx={{ justifyContent: "flex-end" }}>
+                      <Stack
+                        flexDirection="row"
+                        justifyContent="space-between"
                       >
                         <Typography
-                          startDecorator={<DescriptionOutlinedIcon />}
-                          textColor="neutral.300"
+                          level="h2"
+                          fontSize="lg"
+                          textColor="#fff"
+                          mb={1}
                         >
-                          This is delicious meal
+                          {ele.productName}
                         </Typography>
-                      </CardOverflow>
+                        <Typography
+                          sx={{
+                            fontWeight: "md",
+                            color: "neutral.300",
+                            alignItems: "center",
+                            display: "flex",
+                          }}
+                        >
+                          20
+                          <VisibilityIcon
+                            sx={{ fontSize: 25, marginLeft: "5px" }}
+                          />
+                        </Typography>
+                      </Stack>
+                      <Stack>
+                        <CardContent>
+                          <CardOverflow
+                            sx={{
+                              display: "flex",
+                              gap: 1.5,
+                              py: 1.5,
+                              px: "var(--Card-padding)",
+                              borderTop: "1px solid",
+                              height: "60px",
+                            }}
+                          >
+                            <Typography
+                              startDecorator={<DescriptionOutlinedIcon />}
+                              textColor="neutral.300"
+                            >
+                              This is delicious meal
+                            </Typography>
+                          </CardOverflow>
+                        </CardContent>
+                      </Stack>
                     </CardContent>
-                  </Stack>
-                </CardContent>
-              </Card>
-            </CssVarsProvider>
-          );
-        })}
-      </Stack>
-    </Stack>
-  </Container>
-</div>
+                  </Card>
+                </CssVarsProvider>
+              ))
+            ) : (
+              <Box className="no-data">Popular dishes are not available!</Box>
+            )}
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
   );
 }
