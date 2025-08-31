d6fb13c4c1aa8978fb30f59071dc6e5ddbe9adb4
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-09-01 01:45:56 +0900

    feat: develop logic of adding products into basket

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
 import  HomePage from "./screens/homePage";
 import HelpPage from "./screens/helpPage";
@@ -11,19 +11,45 @@ import Footer from "./components/footer";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
+import { CartItem } from "../lib/types/search";
 
 
 function App() {
 const location = useLocation();
 
+const cartJson: string | null = localStorage.getItem("cartData");
+const currentCart = cartJson ? JSON.parse(cartJson) : [];
+const [cartItems, setCartItems] = useState<CartItem[]>(currentCart);
+
+/** HANDLERS **/
+
+const onAdd = (input: CartItem) => {
+    const exist: any = cartItems.find(
+        (item: CartItem) => item._id === input._id
+    );
+    if (exist) {
+        const cartUpdate = cartItems.map((item: CartItem) =>
+            item._id === input._id
+                ? { ...exist, quantity: exist.quantity + 1 }
+                : item
+        );
+        setCartItems(cartUpdate);
+        localStorage.setItem("cartData", JSON.stringify(cartUpdate));
+    } else {
+        const cartUpdate = [...cartItems, { ...input, quantity: 1 }];
+        setCartItems(cartUpdate);
+        localStorage.setItem("cartData", JSON.stringify(cartUpdate));
+    }
+}
+
 
 
 return (
         <>
-        {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
+        {location.pathname === "/" ? <HomeNavbar cartItems={cartItems} /> : <OtherNavbar cartItems={cartItems} />}
         <Switch>
           <Route path="/products">
-            <ProductsPage />
+            <ProductsPage onAdd={onAdd} />
           </Route>
           <Route path="/orders">
             <OrdersPage />
--- src/app/screens/productsPage/ChosenProduct.tsx
+++ src/app/screens/productsPage/ChosenProduct.tsx
@@ -21,6 +21,7 @@ import { useParams } from "react-router-dom";
 import ProductService from "../../services/ProductService";
 import MemberService from "../../services/MemberService";
 import { serverApi } from "../../../lib/config";
+import { CartItem } from "../../../lib/types/search";
 
 /** REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
@@ -40,7 +41,13 @@ const restaurantRetriever = createSelector(
   })
 );
 
-export default function ChosenProduct() {
+interface ChosenProductProps {
+  onAdd: (item: CartItem) => void;
+}
+
+
+export default function ChosenProduct(props: ChosenProductProps) {
+  const { onAdd } = props;
   const { productId } = useParams<{ productId: string }>();
   const { setRestaurant, setChosenProduct } = actionDispatch(useDispatch());
   const { chosenProduct } = useSelector(chosenProductRetriever);
